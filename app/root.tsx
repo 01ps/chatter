72f809de5c10354e277fac31c0e97f578200573d
scordep <sales.pedro@proton.me>
2024-11-08 13:31:52 -0300

    First commit

--- app/root.tsx
+++ app/root.tsx
@@ -4,10 +4,23 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
+  useLoaderData,
+  useRevalidator,
 } from "@remix-run/react";
-import type { LinksFunction } from "@remix-run/node";
+import { LinksFunction, LoaderFunctionArgs, json } from "@remix-run/node";
 
 import "./tailwind.css";
+import { createBrowserClient } from "@supabase/auth-helpers-remix";
+import { SupabaseClient } from "@supabase/supabase-js";
+import { useEffect, useState } from "react";
+import createServerSupabase from "utils/supabase.server";
+import type { Database } from "db_types";
+
+type TypedSupabaseClient = SupabaseClient<Database>;
+
+export type SupabaseOutletContext = {
+  supabase: TypedSupabaseClient;
+};
 
 export const links: LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
@@ -21,6 +34,22 @@ export const links: LinksFunction = () => [
     href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
   },
 ];
+
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+  const env = {
+    SUPABASE_URL: process.env.SUPABASE_URL!,
+    SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY!,
+  };
+
+  const response = new Response();
+  const supabase = createServerSupabase({ request, response });
+
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
+  return json({ env, session }, { headers: response.headers });
+};
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
@@ -41,5 +70,28 @@ export function Layout({ children }: { children: React.ReactNode }) {
 }
 
 export default function App() {
-  return <Outlet />;
+  const { env, session } = useLoaderData<typeof loader>();
+  const revalidator = useRevalidator();
+
+  const [supabase] = useState(() =>
+    createBrowserClient<Database>(env.SUPABASE_URL, env.SUPABASE_ANON_KEY)
+  );
+
+  const serverAccessToken = session?.access_token;
+
+  useEffect(() => {
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((event, session) => {
+      if (session?.access_token !== serverAccessToken)
+        // call loaders
+        revalidator.revalidate();
+    });
+
+    return () => {
+      subscription.unsubscribe();
+    };
+  }, [supabase, revalidator, serverAccessToken]);
+
+  return <Outlet context={{ supabase }} />;
 }
